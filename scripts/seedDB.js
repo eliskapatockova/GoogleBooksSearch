787068c181b68065df38fb14de55d7712d16600a
Eliska Patockova <patockovaeliska@gmail.com>
2020-07-02 12:23:25 -0700

    edited seedDB sample Book

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -11,7 +11,7 @@ mongoose.connect(
 const bookSeed =
 {
     authors: ["Suzanne Collins"],
-    description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    description: "Could you survive on your own, in the wild, with everyone fighting against you? Twenty-four are forced to enter. Only the winner survives. In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. Each year, the districts are forced by the Capitol to send one boy and one girl between the ages of twelve and eighteen to participate in the Hunger Games, a brutal and terrifying fight to the death -- televised for all of Panem to see. Survival is second nature for sixteen-year-old Katniss Everdeen, who struggles to feed her mother and younger sister by secretly hunting and gathering beyond the fences of District 12. When Katniss steps in to take the place of her sister in the Hunger Games, she knows it may be her death sentence. If she is to survive, she must weigh survival against humanity and life against love.",
     image: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
     link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
     title: "The Hunger Games",
